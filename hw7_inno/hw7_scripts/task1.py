9404c966ca9ce1e295a09738c28d8134b7a38844
Егор Алексеенко <sapplementary@gmail.com>
2026-08-28 22:12:51 +0300

    I fixed the method and added spaces around ‘ | ’

--- hw7_inno/hw7_scripts/task1.py
+++ hw7_inno/hw7_scripts/task1.py
@@ -16,11 +16,11 @@ user_data[0] = f"UID-{user_data[0]}"
 user_data[1] = user_data[1].replace('_', ' ').title()
 
 #the 5th step
-user_data[2] = user_data[2].capitalize()
+user_data[2] = user_data[2].upper()
 
 #the 6th step
 user_data[3] = user_data[3].lower()
 
 #the 7th step
-print('|'.join(user_data))
+print(' | '.join(user_data))
 
